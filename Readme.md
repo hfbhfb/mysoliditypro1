d550056694754bc7227177097d30c95c8157dd0b
hefabao <hefabao@126.com>
2022-01-21 21:50:42 +0800

    提交我的helloworld的合约solidity

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+* 版本选择 (使用最新的helloworld会报错) 
+  * pragma solidity ^0.4.25;
+  * 
+
+
---
+++ examples/1helloworld.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.25;
+
+contract helloworld {
+    function hi() public pure returns (string) {
+        return "Hello world";
+    }
+}
